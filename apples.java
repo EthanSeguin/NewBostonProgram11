d4bbedda58b1c7c7697bae79bab995a98ee47576
Ethan Seguin <ethan.seguin@techhigh.us>
2015-08-25 21:38:48 -0400

    Apples

---
+++ apples.java
@@ -0,0 +1,17 @@
+
+class apples {
+public static void main (String args[]){
+	
+	int boy, girl;
+	boy = 11;
+	girl = 68;
+	
+	if  ( boy > 10 || girl < 60){
+		System.out.println("You can enter");
+	}
+	else {
+		System.out.println("You can not enter");
+	}
+	
+}
+}
